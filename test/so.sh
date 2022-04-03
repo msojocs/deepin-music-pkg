1d98bbcbc34b39f723244fcbc0294ce7454e877d
msojocs <jiyecafe@gmail.com>
2022-04-03 11:10:21 +0800

    update: 优化库安装逻辑

--- test/so.sh
+++ test/so.sh
@@ -12,3 +12,24 @@ echo "==========for============"
 for item in $result;do
   echo $item
 done
+
+
+# test
+install_lib (){
+  lib_name=$1
+  echo "find lib: $lib_name"
+
+  for path in /usr/lib/x86_64-linux-gnu /usr/lib /lib/x86_64-linux-gnu /opt/Qt/5.15.2/gcc_64/lib;do
+    if ls "$path/$lib_name"* 1>/dev/null 2>&1;then
+      echo "find $path"
+      return
+    fi
+  done
+  echo "库文件不存在 0"
+  
+}
+
+install_lib libffi
+if ls "/usr/lib/libffi"* 1>/dev/null 2>&1;then
+  echo "ok"
+fi
